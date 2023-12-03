c3d0525b917900174d7511129640011dfe67ebc7
Stephen <teamgreensf@gmail.com>
2023-12-03 13:36:07 -0800

    Custom theme support

--- docker/x11-vnc/entrypoint.sh
+++ docker/x11-vnc/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -ex
 
+if [ "$THEME" ]; then 
+  mkdir -p /root/.fluxbox/styles
+  cp -r "/root/.fluxbox-themes/$THEME"/* /root/.fluxbox/styles
+  mv /root/.fluxbox/styles/bg.png /root/.fluxbox/bg.png
+fi
+
 exec supervisord -c /supervisord.conf
